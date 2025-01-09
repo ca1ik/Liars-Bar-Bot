4f847be7ac4fbf5fadbd71ac7ddf4fd98266edb1
Halil Calik <halicalix@gmail.com>
2025-01-10 02:58:40 +0300

    create ifea

---
+++ Features/ifeature.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ub0.features
+{
+	// Token: 0x0200000B RID: 11
+	public interface ifeature
+	{
+		// Token: 0x06000038 RID: 56
+		void on_render();
+
+		// Token: 0x06000039 RID: 57
+		void on_update();
+	}
+}
